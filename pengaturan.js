c99958f912fbe431a0b4ac6e384711083e8933a3
jamsproject <jaminrendy@gmail.com>
2026-08-11 22:14:44 +0700

    Update pengaturan.js

--- pengaturan.js
+++ pengaturan.js
@@ -9,5 +9,5 @@ const CONFIG_APP = {
 
     // 2. Google Drive API Key (Penting agar galeri bisa memuat foto)
     googleApiKey: "AIzaSyDm91Sn5NLjiCyaX-q7LcsIIFwGDl1uZFI",
-    whatsappAdmin: "62821000000000" // <-- TAMBAHKAN BARIS INI (Ganti dengan nomor WA admin)
+    whatsappAdmin: "6281382016527" // <-- TAMBAHKAN BARIS INI (Ganti dengan nomor WA admin)
 };
